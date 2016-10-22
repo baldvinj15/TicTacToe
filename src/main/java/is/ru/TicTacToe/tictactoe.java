335572a06df104d6fdf16aac715014dca261d902
Svavarberg <svavarberg7@gmail.com>
2016-10-22 15:01:15 +0000

    resetfall

--- src/main/java/is/ru/TicTacToe/tictactoe.java
+++ src/main/java/is/ru/TicTacToe/tictactoe.java
@@ -13,6 +13,14 @@ public class TicTacToe {
 		
 	}
 
+	public void resetGrid(){
+		for (int i = 0;i < 3; i++) {
+			for (int j = 0;j < 3; j++) {
+				grid[i][j];
+			}
+		}
+	}
+
 	private int grid [][];
 
 
