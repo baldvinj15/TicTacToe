8d204204db5f2c96f01cca9d751b3aa72bbcc373
Svavarberg <svavarberg7@gmail.com>
2016-10-22 15:58:59 +0000

    Update0.1

--- src/main/java/is/ru/TicTacToe/tictactoe.java
+++ src/main/java/is/ru/TicTacToe/tictactoe.java
@@ -9,16 +9,22 @@ public class TicTacToe {
 	public static int player1 = 0;
 	public static int player2 = 1;
 
+	public TicTacToe{
+		resetGrid();
+		printGrid();
+	}
+
 	public static void printGrid() {
-		for (int i = 0;i < 3;i++ ) {
-			for (int j = 0;j < 3;j++ ) {
+		for (int i = 0;i < ROWS;i++ ) {
+			for (int j = 0;j < COLS;j++ ) {
+				grid[i][j] = 'Ω';
 				System.out.printGrid[i][j];
 			}
 			System.out.println();
 		}
 	}
 
-	public void resetGrid(){
+	public static void resetGrid(){
 		for (int i = 0;i < 3; i++) {
 			for (int j = 0;j < 3; j++) {
 				grid[i][j];
