05281c833a502b8904531ab56ac2f88f79e2d2a0
sverrirp15 <sverrirp15@ru.is>
2016-10-22 14:51:36 +0000

    bætti við breytum

--- src/main/java/is/ru/TicTacToe/tictactoe.java
+++ src/main/java/is/ru/TicTacToe/tictactoe.java
@@ -2,19 +2,21 @@ package is.ru.TicTacToe
 
 public class TicTacToe {
 	
-<<<<<<< HEAD
 	public static int ROWS = 3;
 	public static int COLS = 3;
 	private int[][] grid = new grid[ROWS][COLS];
+	public boolean winner = false;
+	public static int player1 = 0;
+	public static int player2 = 1;
 
 	public static void printGrid() {
 		
 	}
-=======
+
 	private int grid [][];
 
 
 	
 	
->>>>>>> 86493a710020250d7f8ad1b7006f65a6467822d0
+
 }
